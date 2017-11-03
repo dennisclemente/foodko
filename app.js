b25c62d96d44e021b3e7703620e8f89629f9c84b
Dennis Clemente <DennisClemente@gmail.com>
2017-11-02 18:39:05 -0700

    setup auth

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -7,9 +14,12 @@ app.get('/', (req, res) => {
     res.send('It works!');
 });
 
+//Use Routes
+app.use('/auth', auth);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port `+ port)
+    console.log(`Server started on port ` + port)
 });
 
