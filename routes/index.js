03e3a65f241f25fa3c930342ed987d10f05e6ade
Dennis Clemente <dennisclemente@gmail.com>
2017-11-03 21:59:37 -0700

    started IO

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('It works!');
+    res.render('index/welcome');
 });
 
 router.get('/dashboard', (req, res) => {
