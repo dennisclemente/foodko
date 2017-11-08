2048ac1ed08c4ecb05edf6cd08f5e8bca3427c8d
Dennis Clemente <dennisclemente@gmail.com>
2017-11-08 07:52:38 -0800

    fixed keys errors

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-// if(process.env.NODE_env === 'production'){
-//     module.exports =require('./keys_prod');
-// } else {
-// module.exports = require('./keys_dev');
-// }
-
 module.exports = {
     mongoURI: 'mongodb://dcatucb:dcCncucb1995@ds243295.mlab.com:43295/cook',
     googleClientID:'871360875664-43v9si2lutgji8fke92m59jlsb3j0cjo.apps.googleusercontent.com',
