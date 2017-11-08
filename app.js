6fbbf388768abd8933ba7398c76e97766e152b26
Dennis Clemente <dennisclemente@gmail.com>
2017-11-08 14:39:26 -0800

    fixed functionalities

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use('/', index);
 app.use('/auth', auth);
 app.use('/stories', stories);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
